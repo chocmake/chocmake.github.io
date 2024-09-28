4a78b31920e16faa50b6cb96aa7b4e5b3108370b
chocmake <34178938+chocmake@users.noreply.github.com>
2024-09-28 14:41:58 +0000

    New session and fragment link handling, article

--- js/session.js
+++ js/session.js
@@ -1,151 +1,115 @@
-/**
- * Implements JSON stringify and parse functions
- * v1.0
- *
- * By Craig Buckler, Optimalworks.net
- *
- * As featured on SitePoint.com
- * Please use as you wish at your own risk.
-*
- * Usage:
- *
- * // serialize a JavaScript object to a JSON string
- * var str = JSON.stringify(object);
- *
- * // de-serialize a JSON string to a JavaScript object
- * var obj = JSON.parse(str);
- */
+// Strip `no-js` class from html
+const html = document.documentElement;
+html.classList.remove('no-js');
 
-var JSON = JSON || {};
-
-// implement JSON.stringify serialization
-JSON.stringify = JSON.stringify || function (obj) {
-
-	var t = typeof (obj);
-	if (t != "object" || obj === null) {
-
-		// simple data type
-		if (t == "string") obj = '"'+obj+'"';
-		return String(obj);
-
-	}
-	else {
-
-		// recurse array or object
-		var n, v, json = [], arr = (obj && obj.constructor == Array);
-
-		for (n in obj) {
-			v = obj[n]; t = typeof(v);
-
-			if (t == "string") v = '"'+v+'"';
-			else if (t == "object" && v !== null) v = JSON.stringify(v);
-
-			json.push((arr ? "" : '"' + n + '":') + String(v));
-		}
-
-		return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-	}
-};
-
-
-// implement JSON.parse de-serialization
-JSON.parse = JSON.parse || function (str) {
-	if (str === "") str = '""';
-	eval("var p=" + str + ";");
-	return p;
-};
-
-/**
- * Implements cookie-less JavaScript session variables
- * v1.0
- *
- * By Craig Buckler, Optimalworks.net
- *
- * As featured on SitePoint.com
- * Please use as you wish at your own risk.
-*
- * Usage:
- *
- * // store a session value/object
- * Session.set(name, object);
- *
- * // retreive a session value/object
- * Session.get(name);
- *
- * // clear all session data
- * Session.clear();
- *
- * // dump session data
- * Session.dump();
- */
-
- if (JSON && JSON.stringify && JSON.parse) var Session = Session || (function() {
-
-	// window object
-	var win = window.top || window;
-
-	// session store
-	var store = (win.name ? JSON.parse(win.name) : {});
-
-	// save store on page unload
-	function Save() {
-		win.name = JSON.stringify(store);
-	};
-
-	// page unload event
-	if (window.addEventListener) window.addEventListener("unload", Save, false);
-	else if (window.attachEvent) window.attachEvent("onunload", Save);
-	else window.onunload = Save;
-
-	// public methods
-	return {
-
-		// set a session variable
-		set: function(name, value) {
-			store[name] = value;
-		},
-
-		// get a session value
-		get: function(name) {
-			return (store[name] ? store[name] : undefined);
-		},
-
-		// clear session
-		clear: function() { store = {}; },
-
-		// dump session data
-		dump: function() { return JSON.stringify(store); }
-
-	};
-
- })();
-
-
-// [choc]
-// Handles persistence of user settings across page reloads, lasting only until the tab is closed
-// Works with cookies/local storage disabled while not storing anything to disk
-window.onload = function() {
-    // Obtain all user setting checkboxes
-    var userSettings = document.querySelectorAll(".user-setting[type='checkbox']");
-
-    for (var i = 0; i < userSettings.length; i++) {
-        // Grab the element ID
-        var id = userSettings[i].getAttribute('id');
-        var el = document.querySelector('.user-setting#' + id)
-        // Check whether the setting has been defined previously
-        if (Session.get(id) == "checked") {
-            el.checked = true;
-        }
-        // Listen for subsequent changes
-        userSettings[i].addEventListener("click", checkState);
+// Detect localStorage support
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
     }
-
-    function checkState(e) {
-        if (e.target.checked) {
-            Session.set(e.target.getAttribute('id'), "checked");
-        }
-        else {
-            Session.set(e.target.getAttribute('id'), "unchecked");
-        }
+    catch(e) {
+        return false;
     }
-};
+}
+
+if (storageAvailable('localStorage')) {
+    var Session = Session || (function() {
+        return {
+            set: function(name, value) {
+                window.localStorage.setItem(name, value);
+            },
+
+            get: function(name) {
+                return window.localStorage.getItem(name);
+            }
+        };
+    })();
+} else {
+    // Lightly adapted 'Cookie-less JavaScript session variables' by Craig Buckler (optimalworks.net)
+    // Handles persistence of settings across page reloads, lasting only until the tab is closed
+    var Session = Session || (function() {
+    	var win = window.top || window; // window object
+    	var store = (win.name ? JSON.parse(win.name) : {}); // session store
+
+        function Save() {
+    		win.name = JSON.stringify(store);
+    	};
+
+        window.addEventListener('beforeunload', (e) => {
+            Save();
+        });
+
+    	return {
+    		set: function(name, value) {
+    			store[name] = value;
+    		},
+
+    		get: function(name) {
+    			return (store[name] ? store[name] : undefined);
+    		},
+
+    		clear: function() { store = {}; },
+
+    		dump: function() { return JSON.stringify(store); }
+        };
+    })();
+}
+
+// Read theme setting first on page load for adding root class to mitigate some FOUC
+toggleThemeClass(Session.get('theme-toggle [global]') == 'true');
+
+// Detects page state during history navigation so checkbox states don't remain stale from bfcache if the user has changed them in the meantime
+window.addEventListener('pageshow',checkSettingState);
+window.addEventListener('pagehide',checkSettingState);
+
+// Obtain all user setting checkboxes
+var userSettings = document.querySelectorAll(".user-setting[type='checkbox']"),
+    scope;
+
+checkSettingState();
+
+function checkSettingState() {
+    userSettings.forEach(setting => {
+        let id = setting.getAttribute('id');
+        setScope(setting);
+
+        if (Session.get(id + scope) == 'true') {
+            setting.checked = true;
+        } else {
+            setting.checked = false;
+        }
+
+        setting.addEventListener('click',checkState);
+    });
+
+}
+
+function toggleThemeClass(state) {
+    html.classList.toggle('theme-toggle-checked',state);
+}
+
+function setScope(e) {
+    // Set variable naming scope (page vs site-wide)
+    let id = e.getAttribute('id');
+    if (id == 'version-toggle') {
+        scope = ' [' + window.location.pathname + ']';
+    } else {
+        scope = ' ' + '[global]';
+    }
+}
+
+function checkState(e) {
+    let el = e.target,
+        id = el.getAttribute('id');
+    setScope(el);
+    if (el.checked) {
+        Session.set(id + scope, 'true'); // due to localStorage only supporting strings rather than booleans (while the cookie-less method handles it fine) they've been defined as strings to begin with
+    } else {
+        Session.set(id + scope, 'false');
+    }
+    toggleThemeClass(el.checked);
+}
